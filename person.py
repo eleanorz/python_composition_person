3e3fd91ccb5c137229997b5c7aa4bdce2dc49716
elly <eleanorz@gmail.com>
2014-04-21 14:52:53 -0700

    added additional methods and person attributes

--- person.py
+++ person.py
@@ -1,3 +1,16 @@
+def display_greeting():
+	print "Greetings, User!"
+
+display_greeting()
+
+
+name = raw_input("What is your name?")
+print "you are " + name
+# print "you are", name
+age = raw_input("How old are you?")
+print "you are " + age + "years old"
+# print "you are" , age, "years old"
+
 class Person:
 	eyes = "black"
 
@@ -7,6 +20,9 @@ class Person:
 		print "My name is %s" %(name)
 	def age(self, age):
 		print "I am %s years old" %(age)
+	def pet(self, pet):
+		print "My pet's name is %s" %(pet)
+
 
 class Employee(Person):
 	def position(self, position):
@@ -17,3 +33,4 @@ john.gender("male")
 john.name("John")
 john.age(24)
 john.position("janitor")
+john.pet("rufus")
